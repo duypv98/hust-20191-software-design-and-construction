c7e01d55799d621f0331fdfdef5e7d66b6454dc8
Thanh Duy Truong <thanhduyhust@gmail.com>
2019-10-30 22:29:03 +0700

    Add one way trip service: get trip info by id.

---
+++ Construction/Coding/AFC System/src/vn/edu/hust/soict/afc/services/OneWayTripService.java
@@ -0,0 +1,47 @@
+package vn.edu.hust.soict.afc.services;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+
+import vn.edu.hust.soict.afc.common.BaseDataClient;
+import vn.edu.hust.soict.afc.entities.OneWayTrip;
+
+public class OneWayTripService {
+
+	private static BaseDataClient client = new BaseDataClient();
+	
+	/**
+	 * 
+	 * @param ticketCode
+	 * @return OneWayTicket
+	 * @throws SQLException
+	 */
+	public static OneWayTrip getTripInfo(int id) throws SQLException {
+		OneWayTrip oneWayTrip = null;
+		String sql = "SELECT ticket_id, income_station_id, income_time, outcome_station_id, outcome_time, real_fare, ontrip FROM oneway_trip WHERE id = " + id;
+
+		client.open();
+		PreparedStatement ps = client.getConnection().prepareStatement(sql);
+//		ps.setString(1, id);
+
+		ResultSet rs = ps.executeQuery();
+
+		if (rs.first()) {
+			oneWayTrip = new OneWayTrip();
+			oneWayTrip.setId(id);
+			oneWayTrip.setTicketId(rs.getString("ticket_id"));
+			oneWayTrip.setIncomeStationId(rs.getInt("income_station_id"));
+			oneWayTrip.setIncomeTime(rs.getTimestamp("income_time"));
+			oneWayTrip.setOutcomeStationId(rs.getInt("outcome_station_id"));
+			oneWayTrip.setOutcomeTime(rs.getTimestamp("outcome_time"));
+			oneWayTrip.setRealFare(rs.getDouble("real_fare"));
+			oneWayTrip.setOnTrip(rs.getBoolean("ontrip"));
+		}
+//		client.close();
+		return oneWayTrip;
+	}
+	
+}
