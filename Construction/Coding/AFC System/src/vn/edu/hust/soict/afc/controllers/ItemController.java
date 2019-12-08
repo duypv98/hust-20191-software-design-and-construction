1fc1b57e0844a1765ce27b8ee61479ca24fac171
Thanh Duy Truong <thanhduyhust@gmail.com>
2019-12-08 10:59:35 +0700

    Add missing javadoc

--- Construction/Coding/AFC System/src/vn/edu/hust/soict/afc/controllers/ItemController.java
+++ Construction/Coding/AFC System/src/vn/edu/hust/soict/afc/controllers/ItemController.java
@@ -3,8 +3,18 @@ package vn.edu.hust.soict.afc.controllers;
 import vn.edu.hust.soict.afc.common.AppState;
 import vn.edu.hust.soict.afc.common.DataResponse;
 
+/**
+ * 
+ * @author duytruong
+ *
+ */
 public interface ItemController {
-
+	
+	/**
+	 * 
+	 * @param appState
+	 * @return
+	 */
 	DataResponse process(AppState appState);
 
 }
--- Construction/Coding/AFC System/src/vn/edu/hust/soict/afc/utils/NumberRound.java
+++ Construction/Coding/AFC System/src/vn/edu/hust/soict/afc/utils/NumberRound.java
@@ -1,7 +1,17 @@
 package vn.edu.hust.soict.afc.utils;
 
+/**
+ * 
+ * @author duytruong
+ *
+ */
 public class NumberRound {
 
+	/**
+	 * 
+	 * @param number
+	 * @return
+	 */
 	public final static double roundOneDigitAfterAComma(double number) {
 		return Math.round(number * 10) / 10.0;
 	}
