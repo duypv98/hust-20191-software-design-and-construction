b18f42e56b12d093b3f74e6845529590490bbbc3
Duy Cao Van <caovanduy15@gmail.com>
2019-12-26 14:43:07 +0700

    Sửa lại test

--- Construction/Coding/AFC System/src/vn/edu/hust/soict/afc/tests/TFTicketServiceTest.java
+++ Construction/Coding/AFC System/src/vn/edu/hust/soict/afc/tests/TFTicketServiceTest.java
@@ -1,6 +1,6 @@
 package vn.edu.hust.soict.afc.tests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.awt.Color;
 
@@ -8,21 +8,24 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import hust.soict.se.recognizer.TicketRecognizer;
+import vn.edu.hust.soict.afc.DAO.TFTicketDAOImpl;
+import vn.edu.hust.soict.afc.DAO.TFTripDAOImpl;
 import vn.edu.hust.soict.afc.common.DataResponse;
 import vn.edu.hust.soict.afc.entities.Station;
 import vn.edu.hust.soict.afc.services.TFTicketServiceImpl;
 
 /**
- * 
+ *
  * @author duytruong
  *
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TFTicketServiceTest {
-	
+
 //	TwentyFourTicket ticket = new TwentyFourTicket("TF201910300001", "e8dc4081b13434b4", "2019-12-06 07:51:30", false);
 	private String barCode = "abcdefgh";
-	private TFTicketServiceImpl service = new TFTicketServiceImpl();
+	private TFTicketServiceImpl service = new TFTicketServiceImpl(new TFTicketDAOImpl(), new TFTripDAOImpl(), TicketRecognizer.getInstance());
 	private Station incomeStation = new Station(1, "Saint-Lazare", 0);
 	private Station outcomeStation = new Station(3, "Pyramides", 8.5);
 
@@ -31,11 +34,11 @@ public class TFTicketServiceTest {
 		DataResponse response = service.checkIn(barCode, incomeStation);
 		assertEquals(response.getDisplayColor(), Color.GREEN);
 	}
-	
+
 	@Test
 	public void test02CheckOut() {
 		DataResponse response = service.checkOut(barCode, outcomeStation);
 		assertEquals(response.getDisplayColor(), Color.GREEN);
 	}
-	
+
 }
